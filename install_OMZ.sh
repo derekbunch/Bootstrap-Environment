732c546585f678c9236baea1eafe7346846e67fc
Derek Bunch <derekbunch@Dereks-MacBook-Pro.local>
2020-10-21 14:27:01 -0500

    print new lines for progress output

--- install_OMZ.sh
+++ install_OMZ.sh
@@ -1,59 +1,59 @@
 # Install Homebrew if Mac
-echo 'Checking OS version'
+echo; echo 'Checking OS version'
 if [[ "$OSTYPE" == "darwin"* ]]
 then
-  echo 'OS is OS X'
-  echo "Installing Homebrew"
+  echo; echo 'OS is OS X'
+  echo; echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo "Installing zsh"
+  echo; echo "Installing zsh"
   brew install zsh
-  echo "Installing thefuck"
+  echo; echo "Installing thefuck"
   brew install thefuck
 elif [[ "$OSTYPE" == "linux-gnu"* ]]
   then
   is_amazon_linux=$(hostnamectl | grep 'Amazon' | wc -l)
   if (( $is_amazon_linux == 1 ))
     then
-      echo "OS is Amazon Linux"
-      echo "Installing chsh"
+      echo; echo "OS is Amazon Linux"
+      echo; echo "Installing chsh"
       sudo yum -y install util-linux-user
-      echo "Installing zsh"
+      echo; echo "Installing zsh"
       sudo yum -y install zsh
     else
-      echo "OS is Linux"
-      echo "Installing zsh"
+      echo; echo "OS is Linux"
+      echo; echo "Installing zsh"
       sudo apt-get install zsh -y
   fi
 fi
 
 # Install OhMyZsh
-echo "Installing OhMyZsh"
+echo; echo "Installing OhMyZsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Zsh Syntax Highlighting
-echo "Installing Syntax Highlighting"
+echo; echo "Installing Syntax Highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Zsh Autosuggestions
-echo "Installing Autosuggestions"
+echo; echo "Installing Autosuggestions"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Zsh Sync
-echo "Installing Zsh Sync"
+echo; echo "Installing Zsh Sync"
 git clone https://github.com/vickyliin/zsh-sync.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-sync
 
 # Powerlevel10k theme
-echo "Installing Powerlevel10k"
+echo; echo "Installing Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # vi-mode
-echo "Installing vi-mode"
+echo; echo "Installing vi-mode"
 git clone --depth=1 https://github.com/woefe/vi-mode.zsh ~/.zsh/vi-mode.zsh
 
 # Symlink .zshrc
 if test -f ~/.zshrc
 then
-  echo "Renaming existing .zshrc to .zshrc.orig"
+  echo; echo "Renaming existing .zshrc to .zshrc.orig"
   mv ~/.zshrc ~/.zshrc.orig
 fi
 
@@ -61,18 +61,18 @@ while true; do
     read -p "Is this a work device? (y/n): " config_version
     case $config_version in
         [Yy]* )
-          echo "Using work config"
+          echo; echo "Using work config"
           export CONFIG_VERSION='work-config'
           exit;;
         * )
-          echo "Using personal config"
+          echo; echo "Using personal config"
           export CONFIG_VERSION='personal-config'
           exit;;
     esac
 done
 
-echo "Linking zshrc"
+echo; echo "Linking zshrc"
 ln .zshrc ~/.zshrc
 
-echo "Changing shell"
+echo; echo "Changing shell"
 chsh -s $(which zsh)
