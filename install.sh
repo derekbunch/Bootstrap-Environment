88de4576e6a8780ae14ae2eae340db10ad2a84e9
Derek Bunch <derekbunch93@gmail.com>
2021-06-03 21:00:12 +0000

    single parsed folder instead of splitting by type

--- install.sh
+++ install.sh
@@ -122,7 +122,8 @@ select yn in "Yes" "No"; do
     echo -e "\n${Bold}${Blue}Linking tmuxrc${end}"
     ln work-config/.tmux.conf ~/.tmux.conf
     aws s3 sync s3://vuka-ingest-manager/code/derek/ /mnt/data/input/scripts
-    mkdir /mnt/data/input/new /mnt/data/input/new/csv /mnt/data/input/new/json /mnt/data/input/new/sql /mnt/data/input/new/csv/parsed /mnt/data/input/new/json/parsed /mnt/data/input/new/sql/parsed /mnt/data/input/updates /mnt/data/input/updates/parsed
+    # mkdir /mnt/data/input/new /mnt/data/input/new/csv /mnt/data/input/new/json /mnt/data/input/new/sql /mnt/data/input/new/csv/parsed /mnt/data/input/new/json/parsed /mnt/data/input/new/sql/parsed /mnt/data/input/updates /mnt/data/input/updates/parsed
+    mkdir /mnt/data/input/parsed
     break
     ;;
   No)
